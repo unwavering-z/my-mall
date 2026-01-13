4120d42c39d37055032bab82a0fc826e3f287435
unwavering <morehope586@gmail.com>
2026-01-13 22:35:29 +0800

    精修2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  // 彻底精简，只保留 Starlight 核心
   integrations: [
     starlight({
       title: '2026年春节团购',
+      // 关键：告诉项目去哪里找你的自定义 CSS
+      customCss: ['./src/styles/custom.css'],
       sidebar: [
         {
           label: '产品分类',
