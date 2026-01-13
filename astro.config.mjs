ff7bbf67f183eb9a868ae82c9905bfeab4b593e4
unwavering <morehope586@gmail.com>
2026-01-13 21:14:49 +0800

    修改9

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,9 @@ import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 export default defineConfig({
-  // 1. 强制设置尾随斜杠行为，解决 404 常见病根
-  trailingSlash: 'always',
-  
-  // 2. 开启视图过渡
-  viewTransitions: true,
-  
   integrations: [
     starlight({
-      title: '2026春节展示',
+      title: '2026商城展示',
       sidebar: [
         {
           label: '产品分类',
