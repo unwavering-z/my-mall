73490a0bddf5b151e93ed8b922bb621ee9f3899b
unwavering <morehope586@gmail.com>
2026-01-13 23:20:32 +0800

    样式修改5

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,19 +7,10 @@ export default defineConfig({
       title: '2026年春节团购',
       customCss: ['./src/styles/custom.css'],
       sidebar: [
-        // 手动定义品牌分类
-        {
-          label: '感恩册',
-          link: '/', // 首页即是感恩册
-        },
-        {
-          label: '悦享册',
-          link: '/products/yuexiang/', // 指向尚未创建的悦享册页面
-        },
-        {
-          label: '集祥礼',
-          link: '/products/jixiang/',
-        },
+        { label: '感恩册', link: '/' },
+        { label: '悦享册', link: '/products/yuexiang/' },
+        { label: '集祥礼', link: '/products/jixiang/' },
+        { label: '更多好礼', autogenerate: { directory: 'products' } },
       ],
     }),
   ],
