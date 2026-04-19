8d42b07617df0722cf7bfa21f6b5f3b849564e31
zhangyimiao <morehope586@gmail.com>
2026-04-19 14:33:05 +0800

    本地调试并修复了配置文件

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,4 @@
+
+export default new Map([
+["src/content/docs/products/ganen.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fproducts%2Fganen.mdx&astroContentModuleFlag=true")]]);
+		
